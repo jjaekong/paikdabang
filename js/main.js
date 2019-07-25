fbfef1445d0109bac1bc504817ae8b1d609e8e1d
jhkim <jhkim@omnitel.co.kr>
2019-07-25 10:11:13 +0900

    1. 메인 타임딜 추가, 2. 메인 카테고리 영역 높이 반응형, 3. 추가된 알럿, 에러, 4. 구매페이지 도움말 추가

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 (function($) {
 
-    var exchangeTopSwiper, exchangeBottomSwiper, voucherSwiper;
+    var exchangeTopSwiper, voucherSwiper;
 
     $('#exchange-tab').on('show.bs.tab', function (e) {
         $('html, body').addClass('full-height');
@@ -23,18 +23,18 @@
         }
     });
 
-    $('#exchange-tab').on('shown.bs.tab', function (e) {
-        if (exchangeBottomSwiper) {
-            exchangeBottomSwiper.update()
-        } else {
-            exchangeBottomSwiper = new Swiper ('#exchange .bottom-banner.swiper-container', {
-                loop: true,
-                pagination: {
-                    el: '.swiper-pagination',
-                },
-            });
-        }
-    });
+    // $('#exchange-tab').on('shown.bs.tab', function (e) {
+    //     if (exchangeBottomSwiper) {
+    //         exchangeBottomSwiper.update()
+    //     } else {
+    //         exchangeBottomSwiper = new Swiper ('#exchange .bottom-banner.swiper-container', {
+    //             loop: true,
+    //             pagination: {
+    //                 el: '.swiper-pagination',
+    //             },
+    //         });
+    //     }
+    // });
 
     $('#voucher-tab').on('shown.bs.tab', function (e) {
         if (voucherSwiper) {
@@ -50,3 +50,4 @@
     });
 
 })(jQuery);
+

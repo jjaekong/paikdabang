471c6a9d5a3f5c6439fab8ef194fdf6d3e18f262
jjaekong <jjaekong@gmail.com>
2019-10-07 10:04:16 +0900

    배너 3초 롤링, sns 공유 삭제, 구매내역

--- js/main.js
+++ js/main.js
@@ -1,30 +1,57 @@
 (function($) {
 
-    var exchangeTopSwiper, voucherSwiper;
+    var exchangeSwiper, exchangeSwiperIndex = 0;
+    var voucherSwiper, voucherSwiperIndex = 0;
 
     $('#exchange-tab').on('shown.bs.tab', function (e) {
-        if (exchangeTopSwiper) {
-            exchangeTopSwiper.update()
-        } else {
-            exchangeTopSwiper = new Swiper ('#exchange .top-banner.swiper-container', {
+        if (!exchangeSwiper || exchangeSwiper.destroyed) {
+            exchangeSwiper = new Swiper ('#exchange .top-banner.swiper-container', {
                 loop: true,
+                initialSlide: exchangeSwiperIndex,
+                autoplay: {
+                    delay: 3000,
+                },
                 pagination: {
                     el: '.swiper-pagination',
                 },
+                on: {
+                    slideChange: function() {
+                        exchangeSwiperIndex = this.realIndex
+                    }
+                }
             });
         }
     });
 
+    $('#exchange-tab').on('hidden.bs.tab', function (e) {
+        if (exchangeSwiper) {
+            exchangeSwiper.destroy(true, false);
+        }
+    });
+
     $('#voucher-tab').on('shown.bs.tab', function (e) {
-        if (voucherSwiper) {
-            voucherSwiper.update()
-        } else {
+        if (!voucherSwiper || voucherSwiper.destroyed) {
             voucherSwiper = new Swiper ('#voucher .swiper-container', {
                 loop: true,
+                initialSlide: voucherSwiperIndex,
+                autoplay: {
+                    delay: 3000,
+                },
                 pagination: {
                     el: '.swiper-pagination',
                 },
+                on: {
+                    slideChange: function() {
+                        voucherSwiperIndex = this.realIndex
+                    }
+                }
             });
+        }
+    });
+
+    $('#voucher-tab').on('hidden.bs.tab', function (e) {
+        if (voucherSwiper) {
+            voucherSwiper.destroy(true, false);
         }
     });
 
